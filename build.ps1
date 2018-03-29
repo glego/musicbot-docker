e85c71535e4e77e78d95c1812e9142be2c40733e
Glenn Goffin <glenn.goffin@gmail.com>
2018-03-30 00:26:09 +0200

    Removed pyenv: obsolete

--- build.ps1
+++ build.ps1
@@ -1,7 +1,5 @@
 $ARG_MUSICBOT_VERSION = "1.9.7"
-$ARG_PYENV_VERSION = "3.6.4"
 docker build -t glego/musicbot:latest `
     -t glego/musicbot:$ARG_MUSICBOT_VERSION `
     --build-arg ARG_MUSICBOT_VERSION=$ARG_MUSICBOT_VERSION `
-    --build-arg ARG_PYENV_VERSION=$ARG_PYENV_VERSION `
     .
