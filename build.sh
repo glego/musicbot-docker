0196c554d661c559320fde23d5b6ee30581c12eb
Glenn Goffin <glenn.goffin@gmail.com>
2018-03-31 01:09:16 +0200

    typo

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ $ARG_MUSICBOT_VERSION = "1.9.7"
 docker build -t glego/musicbot:arm32v6-latest \
     -t glego/musicbot:arm32v6-$ARG_MUSICBOT_VERSION \
     --build-arg ARG_MUSICBOT_VERSION=$ARG_MUSICBOT_VERSION \
-    -f .\dockerfiles\alpine-3.6\arm32v6\Dockerfile .
+    -f ./dockerfiles/alpine-3.6/arm32v6/Dockerfile .
