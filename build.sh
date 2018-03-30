6eb358c1ce5feaf63dfecf387bb2962a85ee23ed
Glenn Goffin <glenn.goffin@gmail.com>
2018-03-31 01:07:48 +0200

    armhf -> arm32v6

---
+++ build.sh
@@ -0,0 +1,7 @@
+$ARG_MUSICBOT_VERSION = "1.9.7"
+
+# arm32v6 builds
+docker build -t glego/musicbot:arm32v6-latest \
+    -t glego/musicbot:arm32v6-$ARG_MUSICBOT_VERSION \
+    --build-arg ARG_MUSICBOT_VERSION=$ARG_MUSICBOT_VERSION \
+    -f .\dockerfiles\alpine-3.6\arm32v6\Dockerfile .
