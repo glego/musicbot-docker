3cf28099bed4c08c50a00294d679929c9f1e1203
Glenn Goffin <glenn.goffin@gmail.com>
2018-04-09 02:02:46 +0200

    Added musicbot-base for arm32v6

---
+++ build-musicbot-base-arm32v6.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+ARG_MUSICBOT_VERSION="1.9.7"
+
+# arm32v6 builds
+docker -t glego/musicbot-base:arm32v6-alpine-3.6-$ARG_MUSICBOT_VERSION \
+    --build-arg ARG_MUSICBOT_VERSION=$ARG_MUSICBOT_VERSION \
+    -f ./dockerfiles/alpine-3.6-base/arm32v6/Dockerfile .
