53220f1fba38990c5ad2f68f153e8212d98e64a1
Glenn Goffin <glenn.goffin@gmail.com>
2018-04-09 13:21:23 +0200

    Added build script

--- build-musicbot-base.ps1
+++
@@ -1,8 +0,0 @@
-$ARG_MUSICBOT_VERSION = "1.9.7"
-
-# x64 builds
-docker build --no-cache `
-        -t glego/musicbot-base:x86_64-alpine-3.6-$ARG_MUSICBOT_VERSION `
-        -t glego/musicbot-base:x86_64-latest `
-        --build-arg ARG_MUSICBOT_VERSION=$ARG_MUSICBOT_VERSION `
-        -f ./dockerfiles/alpine-3.6-base/arm32v6/Dockerfile .
--- build-musicbot.sh
+++ build-musicbot.sh
@@ -1,28 +1,92 @@
 #!/bin/sh
+set -e
 
 ARG_MUSICBOT_VERSION="1.9.7"
+ARG_DIST="alpine-3.6"
 
-# Get CPU Architecture
-ARCH=$(lscpu | grep "Architecture" | awk '{print $2}')
+ARCH=""
+ARG_ARCH=""
+ARG_TAG=""
+ARG_TAG_LATEST=""
 
-echo "Architecture: $ARCH"
-if [ "$ARCH" = "armv7l" ];then
+get_cpu_architecture()
+{
+    # Get CPU Architecture
+    ARCH=$(lscpu | grep "Architecture" | awk '{print $2}')
+    echo "Architecture: $ARCH"
 
-    # arm32v6 musicbot build
+    if [ "$ARCH" = "armv7l" ];then
+        ARG_ARCH="arm32v6"
+        ARG_TAG="$ARG_ARCH-$ARG_DIST-$ARG_MUSICBOT_VERSION"
+        ARG_TAG_LATEST="$ARG_ARCH-latest"
+    elif [ "$ARCH" = "x86_64" ];then
+        ARG_ARCH="x86_64"
+        ARG_TAG="$ARG_DIST-$ARG_MUSICBOT_VERSION"
+        ARG_TAG_LATEST="latest"
+    else
+        echo "ERROR: Could not find any valid architecture..."
+        exit 1 # Exit the script
+    fi
+}
+
+build_musicbot()
+{
+    # musicbot
     docker build --no-cache \
-        -t glego/musicbot:arm32v6-alpine-3.6-$ARG_MUSICBOT_VERSION \
-        -t glego/musicbot:arm32v6-latest \
+        -t glego/musicbot-base:$ARG_TAG \
+        -t glego/musicbot-base:$ARG_TAG_LATEST
         --build-arg ARG_MUSICBOT_VERSION=$ARG_MUSICBOT_VERSION \
-        -f ./dockerfiles/alpine-3.6/arm32v6/Dockerfile .
+        -f ./dockerfiles/$ARG_DIST/$ARG_ARCH/Dockerfile .
 
-elif [ "$ARCH" = "x86_64" ];then
-    # x86_64 musicbot build
+    if [ "$ARCH" = "armv7l" ];then
+
+        # arm32v6 musicbot build
+        docker build --no-cache \
+            -t glego/musicbot:arm32v6-alpine-3.6-$ARG_MUSICBOT_VERSION \
+            -t glego/musicbot:arm32v6-latest \
+            --build-arg ARG_MUSICBOT_VERSION=$ARG_MUSICBOT_VERSION \
+            -f ./dockerfiles/alpine-3.6/arm32v6/Dockerfile . \
+            || build_musicbot_base
+
+    elif [ "$ARCH" = "x86_64" ];then
+        # x86_64 musicbot build
+        docker build --no-cache \
+            -t glego/musicbot:alpine-3.6-$ARG_MUSICBOT_VERSION \
+            -t glego/musicbot:latest \
+            --build-arg ARG_MUSICBOT_VERSION=$ARG_MUSICBOT_VERSION \
+            -f ./dockerfiles/alpine-3.6/x86_64/Dockerfile . \
+            || build_musicbot_base
+
+    else
+        echo "ERROR: Could not find any valid architecture..."
+        exit 1 # Exit the script
+    fi
+}
+
+build_musicbot_base()
+{
+    # musicbot-base
     docker build --no-cache \
-        -t glego/musicbot:x86_64-alpine-3.6-$ARG_MUSICBOT_VERSION \
-        -t glego/musicbot:latest \
+        -t glego/musicbot-base:$ARG_TAG \
+        -t glego/musicbot-base:$ARG_TAG_LATEST
         --build-arg ARG_MUSICBOT_VERSION=$ARG_MUSICBOT_VERSION \
-        -f ./dockerfiles/alpine-3.6/x86_64/Dockerfile .
+        -f ./dockerfiles/$ARG_DIST-base/$ARG_ARCH/Dockerfile .
+}
 
-else
-    echo "Could not find any valid architecture..."
-fi
+# Check if musicbot base needs to be rebuild
+check_musicbot_base()
+{
+    docker run --rm glego/musicbot-base:$ARG_TAG_LATEST cat /app/requirements.sha1 > requirements.sha1 || build_musicbot_base
+    wget "https://raw.githubusercontent.com/Just-Some-Bots/MusicBot/$ARG_MUSICBOT_VERSION/requirements.txt"
+    sha1sum -c requirements.sha1 || build_musicbot_base
+    echo "Checksum OK, no need to rebuild base..."
+}
+
+main()
+{
+    get_cpu_architecture
+    check_musicbot_base
+    build_musicbot
+}
+
+main
