6be3c9f0b7408469342718f6dd089ff22d761f64
Glenn Goffin <glenn.goffin@gmail.com>
2018-04-09 21:37:16 +0200

    Builder cleanup

--- build-musicbot.sh
+++ build-musicbot.sh
@@ -4,10 +4,32 @@ set -e
 ARG_MUSICBOT_VERSION="1.9.7"
 ARG_DIST="alpine-3.6"
 
+IS_FORCED_REBUILD_BASE="true"
+
 ARCH=""
 ARG_ARCH=""
 ARG_TAG=""
 ARG_TAG_LATEST=""
+DOCKER_USERNAME=${DOCKER_USERNAME}
+DOCKER_PASSWORD=${DOCKER_PASSWORD}
+
+SCRIPT=$(readlink -f "$0")
+SCRIPT_PATH=$(dirname "$SCRIPT")
+
+BUILD_PATH=$SCRIPT_PATH
+
+check_attributes(){
+
+    if [ "$DOCKER_USERNAME" == "" ];then
+        echo "ERROR: Please provide a valid docker username."
+        exit 1
+    fi
+
+    if [ "$DOCKER_PASSWORD" == "" ];then
+        echo "ERROR: Please provide a valid docker password."
+        exit 1
+    fi
+}
 
 get_cpu_architecture()
 {
@@ -29,51 +51,81 @@ get_cpu_architecture()
     fi
 }
 
-build_musicbot()
-{
-    echo "Building musicbot"
+print_build_banner(){
+    echo "********************************"
+    echo "Building $1"
     echo "Distribution: $ARG_DIST"
     echo "Architecture: $ARG_ARCH" 
     echo "Musicbot: $ARG_MUSICBOT_VERSION"
     echo "Tag: $ARG_TAG" 
-    echo "Tag: $ARG_TAG_LATEST" 
+    echo "Tag: $ARG_TAG_LATEST"
+    echo "********************************"
+    echo "                                "
+}
 
+build_musicbot()
+{
+    print_build_banner "musicbot"
+
+    cd "$BUILD_PATH"
     # musicbot
     docker build --no-cache \
-        -t glego/musicbot-base:$ARG_TAG \
-        -t glego/musicbot-base:$ARG_TAG_LATEST \
+        -t glego/musicbot:$ARG_TAG \
+        -t glego/musicbot:$ARG_TAG_LATEST \
         --build-arg ARG_MUSICBOT_VERSION=$ARG_MUSICBOT_VERSION \
         -f ./dockerfiles/$ARG_DIST/$ARG_ARCH/Dockerfile .
+    
+    docker_push "musicbot"
 }
 
 build_musicbot_base()
 {
-    echo "Building musicbot base"
-    echo "Distribution: $ARG_DIST"
-    echo "Architecture: $ARG_ARCH" 
-    echo "Musicbot: $ARG_MUSICBOT_VERSION"
-    echo "Tag: $ARG_TAG" 
-    echo "Tag: $ARG_TAG_LATEST" 
+    print_build_banner "musicbot-base"
 
+    cd "$BUILD_PATH"
     # musicbot-base
     docker build --no-cache \
         -t glego/musicbot-base:$ARG_TAG \
         -t glego/musicbot-base:$ARG_TAG_LATEST \
         --build-arg ARG_MUSICBOT_VERSION=$ARG_MUSICBOT_VERSION \
         -f ./dockerfiles/$ARG_DIST-base/$ARG_ARCH/Dockerfile .
+    
+    docker_push "musicbot-base"
 }
 
 # Check if musicbot base needs to be rebuild
 check_musicbot_base()
 {
-    docker run --rm glego/musicbot-base:$ARG_TAG_LATEST cat /app/requirements.sha1 > requirements.sha1 || build_musicbot_base
+    if [ "$IS_FORCED_REBUILD_BASE" = "true" ];then
+        echo "Force rebuilding base..."
+        build_musicbot_base
+        return 0
+    fi
+    rm -rf "/tmp/"*
+    cd "/tmp/"
+    docker run --rm glego/musicbot-base:$ARG_TAG_LATEST cat /app/requirements.sha1 > ./requirements.sha1 || build_musicbot_base
     wget "https://raw.githubusercontent.com/Just-Some-Bots/MusicBot/$ARG_MUSICBOT_VERSION/requirements.txt"
     sha1sum -c requirements.sha1 || build_musicbot_base
     echo "Checksum OK, no need to rebuild base..."
+    
+}
+
+docker_login ()
+{
+    echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
+}
+
+docker_push ()
+{
+    repo=$1
+    docker_login
+    docker push glego/$repo:$ARG_TAG
+    docker push glego/$repo:$ARG_TAG_LATEST
 }
 
 main()
 {
+    check_attributes
     get_cpu_architecture
     check_musicbot_base
     build_musicbot
